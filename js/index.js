4bbb5051a3b9e3b0a500b2c76969d693d0a1171b
Reo <taufiknestle151@gmail.com>
2026-02-07 17:27:37 +0700

    Update index.js
    Signed-off-by: Reo <taufiknestle151@gmail.com>

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const lock = document.getElementById("lock");
 const countdown = document.getElementById("countdown");
 const timerEl = document.getElementById("timer");
 const lockMsg = document.getElementById("lockMsg");
+const successText = document.querySelector(".success");
 
 document.getElementById("unlockBtn").onclick = () => {
   const val = document.getElementById("password").value;
@@ -11,7 +12,12 @@ document.getElementById("unlockBtn").onclick = () => {
   if (val === PASSWORD) {
     lock.classList.add("hidden");
     countdown.classList.remove("hidden");
-    timerEl.textContent = "Berhasil dibuka 💙";
+
+    timerEl.textContent = "• • •";
+
+    setTimeout(() => {
+      successText.style.opacity = "1";
+    }, 500);
 
     setTimeout(() => {
       window.location.href = "gift.html";
